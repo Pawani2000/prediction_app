221130aaa4759ce04c110b2ce624926339e576bb
Pawani Nimasha <143385983+Pawani2000@users.noreply.github.com>
2024-09-13 20:20:35 +0530

    Update home.py

--- home.py
+++ home.py
@@ -12,12 +12,8 @@ def show_home():
     This interactive application is designed to assist you in predicting the market price of your car and exploring detailed car data. Whether you’re considering buying a Toyota Camry, selling a Honda Civic, or analyzing the market for a Ford Mustang, this app provides accurate price predictions and valuable insights. By inputting details like the car model, year, mileage, and other specifications, you can estimate the selling price with our advanced machine learning model. Additionally, explore trends and patterns in car data, such as average prices for different brands like Hyundai, BMW, or Mercedes-Benz, and discover the distribution of car prices across various models. This app is an essential tool for car enthusiasts, buyers, and sellers alike, offering a comprehensive view of the car market.
     </div>
     """, unsafe_allow_html=True)
-    image_path = r'C:\Users\pcone\Downloads\car\OIP.jpg'
-    
-    if os.path.exists(image_path):
-        st.image(image_path, caption='Explore and Predict Car Prices', use_column_width=True)
-    else:
-        st.error(f"Error: The image file '{image_path}' does not exist.")
+    image_url = "https://drive.google.com/uc?export=view&id=16vTLJvaucpdgvLhvPtSr6OJ5jta78c5e"
+    st.image(image_url, caption='Explore and Predict Car Prices', use_column_width=True)
 
 if __name__ == "__main__":
     show_home()
